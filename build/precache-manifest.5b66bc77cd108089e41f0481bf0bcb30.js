b71965aca637a0adcc6b7decd12673a47ae92b49
Unknown <ianabildskou@gmail.com>
2019-06-03 23:36:17 +0200

    added build tracking

---
+++ build/precache-manifest.5b66bc77cd108089e41f0481bf0bcb30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "494957897d5a33e1e5182134005d541c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf73c6211d67f98445f",
+    "url": "/static/css/main.f5832583.chunk.css"
+  },
+  {
+    "revision": "e6a381d6fbfd1a6014d0",
+    "url": "/static/js/2.a38dc3f8.chunk.js"
+  },
+  {
+    "revision": "fcf73c6211d67f98445f",
+    "url": "/static/js/main.704240a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
