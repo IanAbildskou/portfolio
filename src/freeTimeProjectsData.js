f8a656c1b33dde0e62e137f2a55311a731d369af
Unknown <ianabildskou@gmail.com>
2019-07-11 19:10:39 +0200

    updated assets refs

--- src/freeTimeProjectsData.js
+++ src/freeTimeProjectsData.js
@@ -1,6 +1,6 @@
 export default [
   {
-    img: 'http://ianvictor.dk/img/initiativet.jpg',
+    img: 'https://ianvictor.dk/img/initiativet.jpg',
     title: 'Initiativet',
     text: "In 2017 a former colleague from ITU started a political party proposing direct democracy. The plan was to build a platform where people could follow and vote on proposals currently in the Danish parliament.<br><br>Really interesting project, that had a lot of traction in the media. I volunteered to build the back end as well as integrations to the parliaments systems.",
     buttons: [
@@ -10,27 +10,27 @@ export default [
     videoType: 'mp4'
   },
   {
-    img: 'http://ianvictor.dk/img/panamericaBus.PNG',
+    img: 'https://ianvictor.dk/img/panamericaBus.PNG',
     title: 'Panamerica Bus',
     text: "My good friend and former colleague, Hari from Abtion, and I decided to build an online marketplace for bus tickets in Panama. In 2018 it was impossible to buy a bus ticket online in Panama. You had to physically go to the ticket office to do so.<br><br>We thought we could make that experience easier. It turned out that building the platform was the easy part! Setting up reliable payment gateways in central America is tricky."
   },
   {
-    img: 'http://ianvictor.dk/img/itcc.jpg',
+    img: 'https://ianvictor.dk/img/itcc.jpg',
     title: 'ITCC',
     text: "In the spring of 2016 I participated in the IT case competition at ITU with Jakob O. Okkels and Olivia B. Nielsen. The case was about, how TDC, the biggest Danish telco provider, could meet the market with new IT initiatives.<br><br>We suggested, that they create and facilitate a platform, where users can exchange and broker their data in exchange for services, discounts and even money, to third parties.<br><br>We won the competition, and TDC invited us to their headquarters, to pitch the idea to their executive board. It was a fantastic experience, and we hope, they will implement our ideas some day.",
-    videoUrl: 'http://ianvictor.dk/TDC_ ITU hos TDC.mp4',
+    videoUrl: 'https://ianvictor.dk/video/itcc.mp4',
     videoType: 'mp4'
   },
   {
-    img: 'http://ianvictor.dk/img/sips.PNG',
+    img: 'https://ianvictor.dk/img/sips.PNG',
     title: 'Sips',
     text: "My good friend and former colleague, Hari from Abtion, and I built a mobile drinking game. I have not built a piece of software that is easier to get people to interact with.<br><br>Who’d have thought?",
     buttons: [
-      { label: 'Sips', url: 'http://sips.surge.sh/'}
+      { label: 'Sips', url: 'https://sips.surge.sh/'}
     ]
   },
   {
-    img: 'http://ianvictor.dk/img/gyroSynth.png',
+    img: 'https://ianvictor.dk/img/gyroSynth.png',
     title: 'gyroSynth',
     text: "While I was nomadding around California in 2018 I missed my musical instruments back home dearly. I wondered why instruments couldn’t be more portable.<br><br>I had the idea to use a smartphone as a synth controller. A smartphone has so many expressive sensors like the gyroscope, accelerometer touchscreen and so on. It even has speakers!<br><br>I had a lot of fun building and playing with the gyroSynth. It’s very tricky to calibrate the gyroscope across different devices."
   }
--- src/workData.js
+++ src/workData.js
@@ -1,6 +1,6 @@
 export default [
   {
-    img: 'http://ianvictor.dk/img/triggerz.PNG',
+    img: 'https://ianvictor.dk/img/triggerz.PNG',
     title: 'Triggerz',
     text: "In 2017 I was offered the position of first employee in Triggerz, after having been working on it in my free time since, I graduated in the summer of 2016.<br><br>Together with the founders Lars and Flemming, and later on many more colleagues, we built a super cool learning experience platform.<br><br>Since we are a small team I was functioning as full stack developer, user experience designer and account manager. I have learned so much from Lars and Flemming!",
     buttons: [
@@ -8,22 +8,22 @@ export default [
     ]
   },
   {
-    img: 'http://ianvictor.dk/img/tdcfullpage.jpg',
+    img: 'https://ianvictor.dk/img/tdcfullpage.jpg',
     title: 'TDC',
     text: "After the successful case competition at ITU in 2016, I got a job offer from TDC Erhverv!<br><br>I decided to try my hand at the biggest IT company in Denmark.<br><br>There are a LOT of challenges working at a company that size, but once you're on the right track exciting things happen!<br><br>I was functioning as a business analyst and UX designer on our self service platform. Creating an interface for one of the oldest and most complex IT systems in Denmark keeps your mind sharp!",
     buttons: [
       { label: 'TDC', url: 'https://tdc.dk/'}
     ],
-    videoUrl: 'http://ianvictor.dk/TDC_ ITU hos TDC.mp4',
+    videoUrl: 'https://ianvictor.dk/video/itcc.mp4',
     videoType: 'mp4'
   },
   {
-    img: 'http://ianvictor.dk/img/iansDesign.jpg',
+    img: 'https://ianvictor.dk/img/iansDesign.jpg',
     title: "Ian's Design",
     text: "I created my own company, so I could work freelance in the summer of 2016. It was fun working on a lot of smaller projects.<br><br>I was doing web design and development when I came in contact with Lars from Triggerz, who later became my employer."
   },
   {
-    img: 'http://ianvictor.dk/img/abtion.jpg',
+    img: 'https://ianvictor.dk/img/abtion.jpg',
     title: 'Abtion',
     text: "After I came home from Prague in 2015, I got a job at Abtion, a small digital/software house in Frederiksberg. I functioned as a UI and UX designer, but also did websites and graphical stuff.<br><br>It was a great place to work! We had an office dog, named Muffi, in charge of HR, so working conditions were optimal.",
     buttons: [
@@ -33,7 +33,7 @@ export default [
     videoType: 'webm'
   },
   {
-    img: 'http://ianvictor.dk/img/startupyard.png',
+    img: 'https://ianvictor.dk/img/startupyard.png',
     title: 'StartupYard',
     text: "During my studies in Prague in 2015 I worked at a startup accelerator called StartupYard. I got to work with 6 different startups in their 4 months acceleration program.<br><br>I helped them out with websites and graphics. I learned SO much during those 4 months, and it got me really hooked on the whole startup business.",
     buttons: [
@@ -41,18 +41,18 @@ export default [
     ]
   },
   {
-    img: 'http://ianvictor.dk/img/australia.jpg',
+    img: 'https://ianvictor.dk/img/australia.jpg',
     title: 'Cowboy',
     text: "After &Co I decided to go traveling in 2012! I went to Australia for six months, to try my hand at jackarooing, the Australian version of a cow boys.<br><br>These days they don’t use horses very much though. Instead we used motorbikes and helicopters. I was herding cattle in the middle of Queensland in 50 degrees heat. It was quite an adventure!",
-    videoUrl: 'http://ianvictor.dk/aus.mp4',
+    videoUrl: 'https://ianvictor.dk/video/aus.mp4',
     videoType: 'mp4'
   },
   {
-    img: 'http://ianvictor.dk/img/andCo.PNG',
+    img: 'https://ianvictor.dk/img/andCo.PNG',
     title: '&Co',
     text: "Back in 2012 I did a 2 months internship at &Co, an advertisement house in Copenhagen. I helped out with creative work, and got to see the inner workings of the business.<br><br>They didn’t drink or smoke as much as they do on Mad Men, but it was still super cool!",
     buttons: [
-      { label: '&Co', url: 'http://www.andco.dk/'}
+      { label: '&Co', url: 'https://www.andco.dk/'}
     ]
   }
 ];
