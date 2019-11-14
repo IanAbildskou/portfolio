50107b5b338e957bea7a224cc256f70128f9234d
Ian <ianabildskou@gmail.com>
2019-11-14 14:39:24 +0100

    text change

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c98586ecb989ac653e835027b0dc0f0b.js"
+  "/precache-manifest.ef7acef86f93f5d981a6537c19841d7f.js"
 );
 
 self.addEventListener('message', (event) => {
